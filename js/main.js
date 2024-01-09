201ed10858c610349630ff2dc085ccf9d5ba2c92
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-01-09 15:03:46 +0100

    get connection to inputs, button and input values

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 "use strict";
-// testigen
+// Get connection to the inputs for loan, interest and time
+const loanInput = document.getElementById("loan");
+const interestInput = document.getElementById("interest");
+const timeInput = document.getElementById("time");
+// Get connection to submit button
+const submitBtn = document.getElementById("submit-btn");
+submitBtn.addEventListener("click", () => {
+    const loan = loanInput.value;
+    console.log(loan);
+    const interest = interestInput.value;
+    console.log(interest);
+    const time = timeInput.value;
+    console.log(time);
+    // Values need to be converted to number
+});
+// Adding event listener varför behövs denna??
+// loanInput.addEventListener("input", (event) => {
+//   const target = event.target as HTMLInputElement;
+//   console.log("Värde: " + target.value);
+// });
--- ts/main.ts
+++ ts/main.ts
@@ -1,1 +1,24 @@
-// testigen
+// Get connection to the inputs for loan, interest and time
+const loanInput = document.getElementById("loan") as HTMLInputElement;
+const interestInput = document.getElementById("interest") as HTMLInputElement;
+const timeInput = document.getElementById("time") as HTMLInputElement;
+
+// Get connection to submit button
+const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
+
+submitBtn.addEventListener("click", () => {
+  const loan = loanInput.value;
+  console.log(loan);
+  const interest = interestInput.value;
+  console.log(interest);
+  const time = timeInput.value;
+  console.log(time);
+
+  // Values need to be converted to number
+});
+
+// Adding event listener varför behövs denna??
+// loanInput.addEventListener("input", (event) => {
+//   const target = event.target as HTMLInputElement;
+//   console.log("Värde: " + target.value);
+// });

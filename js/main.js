fadb122a184b832a099a45b431164463d32a6054
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-01-10 14:18:08 +0100

    added explanatory messages for user when invalid inputs are submitted

--- js/main.js
+++ js/main.js
@@ -17,14 +17,50 @@ submitBtn.addEventListener("click", () => {
     console.log(interest);
     const time = Number(timeInput.value) * 12;
     console.log(time);
+    const errorMessage = document.createElement("p");
+    const errorMessage2 = document.createElement("p");
+    // Create variable to find the one to delete
+    const children = document.body.querySelectorAll("p");
+    let childToRemove1;
+    let childToRemove2;
     // Check if time is greater than 50 years
     if (time / 12 > 50) {
-        console.log("The time is too long!");
+        // console.log("The time is too long!");
+        errorMessage.innerHTML = `Återbetalningstiden är för lång!`;
+        document.body.appendChild(errorMessage);
+    }
+    else {
+        children.forEach(child => {
+            if (child.textContent === `Återbetalningstiden är för lång!`) {
+                childToRemove1 = child;
+            }
+        });
     }
     // Check if interest is over 40%
     if ((interest * 100 * 12) > 40) {
-        console.log("The interest is too high!");
+        // console.log("The interest is too high!");
+        errorMessage2.innerHTML = `Räntan du har angett är för hög!`;
+        document.body.appendChild(errorMessage2);
+    }
+    else {
+        children.forEach(child => {
+            if (child.textContent === `Räntan du har angett är för hög!`) {
+                childToRemove2 = child;
+            }
+        });
     }
     // Calculate the monthly cost
     console.log(calculateM(loan, interest, time).toFixed(2));
+    // Present results to user if it passed the tests
+    if (!((interest * 100 * 12) > 40) && !(time / 12 > 50)) {
+        if (childToRemove1) {
+            document.body.removeChild(childToRemove1);
+        }
+        if (childToRemove2) {
+            document.body.removeChild(childToRemove2);
+        }
+        const results = document.createElement("p");
+        results.innerHTML = `NURÅ DIN LILLA RÅTTA`;
+        document.body.appendChild(results);
+    }
 });

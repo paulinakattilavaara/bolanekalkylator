592c614450fc7d07bb5701575458fa52dfe70c61
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-01-10 12:51:20 +0100

    added function to calculate monthly cost

--- js/main.js
+++ js/main.js
@@ -5,14 +5,18 @@ const interestInput = document.getElementById("interest");
 const timeInput = document.getElementById("time");
 // Get connection to submit button
 const submitBtn = document.getElementById("submit-btn");
+function calculateM(P, r, n) {
+    return P * r * ((1 + r) ** n) / (((1 + r) ** n) - 1);
+}
 // Adding event listener to button
 submitBtn.addEventListener("click", () => {
     // Values need to be converted from string to number
     const loan = Number(loanInput.value);
     console.log(loan);
-    const interest = Number(interestInput.value);
+    const interest = Number(interestInput.value) / 100 / 12;
     console.log(interest);
-    const time = Number(timeInput.value);
+    const time = Number(timeInput.value) * 12;
     console.log(time);
-    // Calculate
+    // Calculate the monthly cost
+    console.log(calculateM(loan, interest, time));
 });
--- ts/main.ts
+++ ts/main.ts
@@ -6,15 +6,21 @@ const timeInput = document.getElementById("time") as HTMLInputElement;
 // Get connection to submit button
 const submitBtn = document.getElementById("submit-btn") as HTMLButtonElement;
 
+function calculateM(P: number, r: number, n: number): number {
+  return P * r * ((1 + r) ** n) / (((1 + r) ** n) - 1);
+}
+
+
 // Adding event listener to button
 submitBtn.addEventListener("click", () => {
   // Values need to be converted from string to number
   const loan: number = Number(loanInput.value);
   console.log(loan);
-  const interest: number = Number(interestInput.value);
+  const interest: number = Number(interestInput.value) / 100 / 12;
   console.log(interest);
-  const time: number = Number(timeInput.value);
+  const time: number = Number(timeInput.value) * 12;
   console.log(time);
 
-  // Calculate
+  // Calculate the monthly cost
+  console.log(calculateM(loan, interest, time));
 });
